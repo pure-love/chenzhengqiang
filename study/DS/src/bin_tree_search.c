92c618e3ea947151ddd4b3c691fc18954ab565bb
chenzhengqiang <642346572@qq.com>
2015-08-03 07:08:11 +0800

    update the bin_tree_search.c for completing bst_find function either the Makefile

--- study/DS/src/bin_tree_search.c
+++ study/DS/src/bin_tree_search.c
@@ -16,4 +16,16 @@ void bst_delete_tree( SEARCH_TREE search_tree )
     free(search_tree);    
 }
 
+POSITION bst_find( element_t element, SEARCH_TREE search_tree)
+{
+    if( search_tree == NULL )
+	    return NULL;
+    if( element > search_tree->element )
+	    return bst_find(element,search_tree->right);
+    else if( element < search_tree->element )
+	    return bst_find(element,search_tree->left);
+    else
+	    return search_tree;
+}	
+
 
