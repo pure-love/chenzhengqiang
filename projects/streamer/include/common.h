fa5d45416528ba6f91a0d2a745dbb7d88f7fe8cd
Backer <642346572@qq.com>
2015-09-13 23:27:33 -0700

    reload the streamer project

---
+++ projects/streamer/include/common.h
@@ -0,0 +1,46 @@
+/*
+ * @author:chenzhengqiang
+ * @company:swwy
+ * @date:2015/3/27
+ * @modified-date:
+ * @version:1.0
+ * @desc:
+ *  these are the basic headers needed
+ */
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+#include "errors.h"
+#include<cstdio>
+#include<cstdlib>
+#include<stdint.h>
+#include<cctype>
+#include<cstring>
+#include<cstdarg>
+#include<queue>
+#include<string>
+#include<vector>
+#include<map>
+#include<set>
+#include<utility>
+#include<ev++.h>
+#include<unistd.h>
+#include<signal.h>
+#include<sys/socket.h>
+#include<netinet/tcp.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#include<netdb.h>
+#include<fcntl.h>
+#include<pthread.h>
+#include<errno.h>
+#include<assert.h>
+#include<iostream>
+#include<sstream>
+#include<memory>
+#include<fstream>
+#include<sys/select.h>
+extern int errno;
+using std::queue;
+#endif
---
+++ projects/streamer/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * @author:chenzhengqiang
+ * @company:swwy
+ * @date:2015/3/26
+ * @modified-date:
+ * @version:1.0
+ * @desc:
+ * this is the implementation of the streamer based on http
+*/
+
+#include "streamer.h"
+#include "streamerutility.h"
+
+int main( int ARGC, char ** ARGV )
+{
+    int streamer_listen_fd;
+    int state_listen_fd;
+    CMD_OPTIONS cmd_options;
+    handle_cmd_options(ARGC,ARGV,cmd_options);
+    CONFIG config;
+    read_config(cmd_options.config_file.c_str(),config);
+    if( cmd_options.need_print_help )
+    {
+        print_help();
+    }
+
+    if( cmd_options.need_print_version )
+    {
+        print_version(config.version.c_str());
+    }
+
+    streamer_listen_fd = register_tcp_server(config.streamer_ip.c_str(),config.streamer_port);
+    state_listen_fd = register_tcp_server(config.state_ip.c_str(),config.state_port);
+    serve_forever(streamer_listen_fd,state_listen_fd,config);
+    return 0;
+}	
