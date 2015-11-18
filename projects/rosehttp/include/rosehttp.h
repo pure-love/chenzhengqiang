c359d7f0e1c6e0ecd6b3d4d9041e25302d5f774f
Backer <642346572@qq.com>
2015-11-18 09:26:35 +0800

    startup the rosehttp project

---
+++ projects/rosehttp/include/rosehttp.h
@@ -0,0 +1,14 @@
+/*
+*@filename:rosehttp.h
+*@author:chenzhengqiang
+*@start date:2015/11/18 09:25:35
+*@modified date:
+*@desc: 
+*/
+
+
+
+#ifndef _CZQ_ROSEHTTP_H_
+#define _CZQ_ROSEHTTP_H_
+//write the function prototypes or the declaration of variables here
+#endif
---
+++ projects/rosehttp/src/rosehttp.cpp
@@ -0,0 +1,11 @@
+/*
+*@filename:rosehttp.cpp
+*@author:chenzhengqiang
+*@start date:2015/11/18 09:25:35
+*@modified date:
+*@desc: 
+*/
+
+
+
+#include "rosehttp.h"
