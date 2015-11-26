e77824c014ed1c8b1e07de67cd8ddb1086f411d1
Backer <642346572@qq.com>
2015-11-26 11:26:35 +0800

    startup the xtrartmp project

---
+++ projects/xtrartmp/include/xtrartmp.h
@@ -0,0 +1,14 @@
+/*
+*@filename:xtrartmp.h
+*@author:chenzhengqiang
+*@start date:2015/11/26 11:26:16
+*@modified date:
+*@desc: 
+*/
+
+
+
+#ifndef _CZQ_XTRARTMP_H_
+#define _CZQ_XTRARTMP_H_
+//write the function prototypes or the declaration of variables here
+#endif
---
+++ projects/xtrartmp/src/xtrartmp.cpp
@@ -0,0 +1,11 @@
+/*
+*@filename:xtrartmp.cpp
+*@author:chenzhengqiang
+*@start date:2015/11/26 11:26:16
+*@modified date:
+*@desc: 
+*/
+
+
+
+#include "xtrartmp.h"
