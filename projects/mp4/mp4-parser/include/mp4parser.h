d6f903ff64bc76329ae14bd4e60ed69ea6a8ab3b
Backer <642346572@qq.com>
2015-12-17 17:16:32 +0800

    startup the mp4 project

---
+++ projects/mp4/mp4-parser/include/mp4parser.h
@@ -0,0 +1,14 @@
+/*
+*@filename:mp4parser.h
+*@author:chenzhengqiang
+*@start date:2015/12/17 17:12:03
+*@modified date:
+*@desc: 
+*/
+
+
+
+#ifndef _CZQ_MP4PARSER_H_
+#define _CZQ_MP4PARSER_H_
+//write the function prototypes or the declaration of variables here
+#endif
---
+++ projects/mp4/mp4-parser/src/mp4parser.cpp
@@ -0,0 +1,11 @@
+/*
+*@filename:mp4parser.cpp
+*@author:chenzhengqiang
+*@start date:2015/12/17 17:12:03
+*@modified date:
+*@desc: 
+*/
+
+
+
+#include "mp4parser.h"
