7aa7b3616acdaa98e7e07d8d2ed3f589a8efc7e2
Backer <642346572@qq.com>
2016-01-15 16:30:17 +0800

    modify the flv2ts.c for more readable

--- study/MEDIA/open-source/ffmpeg/flv2ts.c
+++ study/MEDIA/open-source/ffmpeg/flv2ts.c
@@ -6,6 +6,7 @@
 @desc:convert flv file to ts files
 */
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <libavformat/avformat.h>
@@ -14,130 +15,125 @@
 #include <libavutil/opt.h>
 
 
+
 #define FORMAT "segment"
-#define OUTPUT_FILE (argv[2])
 #define INPUT_FILE (argv[1])
+#define OUTPUT_FILE (argv[2])
+#define M3U8_FILE (argv[3])
 
+void print_usage(int argc, char **argv)
+{
+	if ( argc != 5)
+	{
+		printf("Usage:%s <flv file> <ts file prefix> <m3u8 prefix> <duration>"\
+			"\n\rExample:./flv2ts anger.flv fuck fuck\n",argv[0]);
+		exit(EXIT_FAILURE);
+	}	
+}
 
-int main(int argc, char *argv[])
+int main(int argc, char * *argv)
 {
 	AVPacket avPacket;
 	AVFormatContext *iFormatContext = NULL;
 	AVFormatContext *oFormatContext = NULL;
-	AVBitStreamFilterContext * vbsf = NULL;
+	AVBitStreamFilterContext * avbsfContext = NULL;
 	
 	int ret = 0;
-	int i = 0;
+	int index = 0;
+
 	//int have_key = 0;
-	static int first_avPacket = 1;
-	const char *input_file = NULL;
-	const char *output_file = NULL;
-
-	if (argc < 3)
-	{
-        input_file = INPUT_FILE;
-        output_file = OUTPUT_FILE;
-        printf("1:%s\n",input_file);
-	}
-	else
-	{
-        input_file = argv[1];
-        output_file = argv[2];
-        printf("2\n");
-	}
-	
+	static int firstAVPacket = 1;
+	char outputFile[30];
+	char m3u8File[30];
+	int duration;
+	print_usage(argc,argv);
+	snprintf(outputFile, sizeof(outputFile), "%s%03d.ts", OUTPUT_FILE);
+	snprintf(m3u8File, sizeof(m3u8File), "%.m3u8",M3U8_FILE);
+	const char *duration = argv[4];
 	av_register_all();
-	if (avformat_open_input(&iFormatContext, input_file, NULL, NULL) < 0) {
-		printf("av_log 1\n");
-		av_log(NULL, AV_LOG_ERROR, "failed to open the file %s\n", input_file);
-		return -ENOENT;
+	if (avformat_open_input(&iFormatContext, INPUT_FILE, NULL, NULL) < 0) 
+	{
+		av_log(NULL, AV_LOG_ERROR, "failed to open the file %s\n", INPUT_FILE);
+		exit(EXIT_FAILURE);
 	}
 
-	if (avformat_find_stream_info(iFormatContext, 0) < 0) {
-		printf("av_log 2\n");
-		av_log(NULL, AV_LOG_ERROR,
-				"Failed to retrieve input stream information\n");
-		return -EINVAL;
+	if (avformat_find_stream_info(iFormatContext, 0) < 0) 
+	{
+		av_log(NULL,  AV_LOG_ERROR, "Failed to retrieve input stream information\n");
+		exit(EXIT_FAILURE);
 	}
 
-	av_dump_format(iFormatContext, 0, input_file, 0);
-
-	if (avformat_alloc_output_context2(&oFormatContext, NULL, FORMAT,
-	output_file) < 0) {
-		printf("av_log 3\n");
-		av_log(NULL, AV_LOG_ERROR,
-				"Cannot open the file %s \n",
-				output_file);
-		return -ENOENT;
+	av_dump_format(iFormatContext, 0, INPUT_FILE, 0);
+	if (avformat_alloc_output_context2(&oFormatContext, NULL, FORMAT, outputFile) < 0) 
+	{
+		av_log(NULL, AV_LOG_ERROR,"Cannot open the file %s \n",outputFile);
+		exit(EXIT_FAILURE);
 	}
 
-	for (i = 0; i < (int)iFormatContext->nb_streams; i++ ) {
-		AVStream *in_stream = iFormatContext->streams[i];
-		AVStream *out_stream = avformat_new_stream(oFormatContext,
-				in_stream->codec->codec);
-		if ( out_stream < 0 ) {
-			printf("av_log 4\n");
-			av_log(NULL, AV_LOG_ERROR,
-					"Alloc new Stream error\n");
-			return -EINVAL;
+	for (index = 0; index < (int)iFormatContext->nb_streams; index++ ) 
+	{
+		AVStream *IStream = iFormatContext->streams[index];
+		AVStream *OStream = avformat_new_stream(oFormatContext, IStream->codec->codec);
+		if ( OStream == 0 ) 
+		{
+			av_log(NULL, AV_LOG_ERROR,"Alloc new Stream Failed\n");
+			exit(EXIT_FAILURE);
 		}
         
-		avcodec_copy_context(oFormatContext->streams[i]->codec,
-				iFormatContext->streams[i]->codec);
+		avcodec_copy_context(oFormatContext->streams[index]->codec,
+						     iFormatContext->streams[index]->codec);
 
-		out_stream->codec->codec_tag = 0;
-		if (oFormatContext->oformat->flags & AVFMT_GLOBALHEADER) {
-			out_stream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
+		OStream->codec->codec_tag = 0;
+		if (oFormatContext->oformat->flags & AVFMT_GLOBALHEADER) 
+		{
+			OStream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
 		}
 	}
 
-	vbsf = av_bitstream_filter_init("h264_mp4toannexb");
+	avbsfContext = av_bitstream_filter_init("h264_mp4toannexb");
+	av_dump_format(oFormatContext, 0, outputFile, 1);
+	av_opt_set(oFormatContext->priv_data, "segment_list", m3u8File, 0);
+	av_opt_set(oFormatContext->priv_data, "segment_time", duration, 0);
 
-	av_dump_format(oFormatContext, 0, output_file, 1);
-
-
-	av_opt_set(oFormatContext->priv_data, "segment_list", "playlist.m3u8", 0);
-	av_opt_set(oFormatContext->priv_data, "segment_time", "10", 0);
-
-	if ((ret = avformat_write_header(oFormatContext, NULL)) < 0) {
-		printf("av_log 6\n");
-		av_log(NULL, AV_LOG_ERROR,
-				"Cannot write the header for the file '%s' ret = %d\n",
-				"fuck.ts", ret);
-		return -ENOENT;
+	if ((ret = avformat_write_header(oFormatContext, NULL)) < 0) 
+	{
+		av_log(NULL, AV_LOG_ERROR,"Cannot write the header for the file '%s' ret = %d\n","fuck.ts", ret);
+		exit(EXIT_FAILURE);
 	}
 
-	while (1) {
+	while (1) 
+	{
 		ret = av_read_frame(iFormatContext, &avPacket);
-		if (ret < 0) {
-			printf("av_log 7\n");
-			av_log(NULL, AV_LOG_ERROR,
-					"read frame error %d \n", ret);
+		if (ret < 0) 
+		{
+			av_log(NULL, AV_LOG_ERROR,"read frame error %d \n", ret);
 			break;
 		}
 
-		if (avPacket.pts == AV_NOPTS_VALUE && first_avPacket) {
+		if (avPacket.pts == AV_NOPTS_VALUE && firstAVPacket) 
+		{
 			avPacket.pts = avPacket.dts;
-			first_avPacket = 0;
+			firstAVPacket = 0;
 		}
 
-		AVStream *in_stream = iFormatContext->streams[avPacket.stream_index];
-		AVStream *out_stream = oFormatContext->streams[avPacket.stream_index];
+		AVStream *IStream = iFormatContext->streams[avPacket.stream_index];
+		AVStream *OStream = oFormatContext->streams[avPacket.stream_index];
 
-		avPacket.pts = av_rescale_q_rnd(avPacket.pts, in_stream->time_base,
-				out_stream->time_base,
+		avPacket.pts = av_rescale_q_rnd(avPacket.pts, IStream->time_base,
+				OStream->time_base,
 				(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-		avPacket.dts = av_rescale_q_rnd(avPacket.dts, in_stream->time_base,
-				out_stream->time_base,
+		avPacket.dts = av_rescale_q_rnd(avPacket.dts, IStream->time_base,
+				OStream->time_base,
 				(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-		avPacket.duration = av_rescale_q(avPacket.duration, in_stream->time_base,
-				out_stream->time_base);
+		avPacket.duration = av_rescale_q(avPacket.duration, IStream->time_base,
+				OStream->time_base);
 		avPacket.pos = -1;
 
-		if (avPacket.stream_index == 0) {
+		if (avPacket.stream_index == 0) 
+		{
 
 			AVPacket favPacket = avPacket;
-			int a = av_bitstream_filter_filter(vbsf, out_stream->codec, NULL,
+			int a = av_bitstream_filter_filter(avbsfContext, OStream->codec, NULL,
 					&favPacket.data, &favPacket.size, avPacket.data, avPacket.size,
 					avPacket.flags & AV_PKT_FLAG_KEY);
 			if(a<0)
@@ -150,10 +146,9 @@ int main(int argc, char *argv[])
 		}
 
 		ret = av_write_frame(oFormatContext, &avPacket);
-		if (ret < 0) {
-			printf("av_log 8\n");
-			av_log(NULL, AV_LOG_ERROR,
-					"Muxing Error\n");
+		if (ret < 0) 
+		{
+			av_log(NULL, AV_LOG_ERROR, "Muxing Error\n");
 			break;
 		}
 
@@ -164,7 +159,7 @@ int main(int argc, char *argv[])
 	avformat_close_input(&iFormatContext);
 	avio_close(oFormatContext->pb);
 	avformat_free_context(oFormatContext);
-	av_bitstream_filter_close(vbsf);
-	vbsf = NULL;
+	av_bitstream_filter_close(avbsfContext);
+	avbsfContext = NULL;
 	return 0;
 }
