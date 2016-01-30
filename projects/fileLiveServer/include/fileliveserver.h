2d4f4f139d41486cf13f7ea244cc234b1e52500f
backer <642346572@qq.com>
2016-01-30 18:37:56 +0800

    startup the fileLiveServer project

---
+++ projects/fileLiveServer/include/fileliveserver.h
@@ -0,0 +1,14 @@
+/*
+*@filename:fileliveserver.h
+*@author:chenzhengqiang
+*@start date:2016/01/30 18:37:10
+*@modified date:
+*@desc: 
+*/
+
+
+
+#ifndef _CZQ_FILELIVESERVER_H_
+#define _CZQ_FILELIVESERVER_H_
+//write the function prototypes or the declaration of variables here
+#endif
---
+++ projects/fileLiveServer/main.cpp
@@ -0,0 +1,18 @@
+/*
+*@filename:main.cpp
+*@author:chenzhengqiang
+*@start date:2016/01/30 18:34:28
+*@modified date:
+*@desc: 
+*/
+
+
+
+#include<iostream>
+using namespace std;
+
+int main( int argc, char ** argv )
+{
+	cout<<"hello"<<endl;
+	return 0;
+}
