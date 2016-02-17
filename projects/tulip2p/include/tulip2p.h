847a9a99ede072ef45dc51149a0240a403b7f7ed
chenzhengqiang <2034234817@qq.com>
2016-02-17 17:42:22 +0800

    startup the tulip2p project

---
+++ projects/tulip2p/include/tulip2p.h
@@ -0,0 +1,14 @@
+/*
+*@filename:tulip2p.h
+*@author:chenzhengqiang
+*@start date:2016/02/17 17:42:08
+*@modified date:
+*@desc: 
+*/
+
+
+
+#ifndef _CZQ_TULIP2P_H_
+#define _CZQ_TULIP2P_H_
+//write the function prototypes or the declaration of variables here
+#endif
---
+++ projects/tulip2p/src/tulip2p.cpp
@@ -0,0 +1,11 @@
+/*
+*@filename:tulip2p.cpp
+*@author:chenzhengqiang
+*@start date:2016/02/17 17:42:08
+*@modified date:
+*@desc: 
+*/
+
+
+
+#include "tulip2p.h"
