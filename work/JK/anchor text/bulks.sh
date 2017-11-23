fc731cd41caf0ec5ddd4c0c5484b8ca5c73dbc66
backer <backer@169kang.com>
2017-11-23 16:27:35 +0800

    add JK

--- work/JK/anchor text/bulks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-nohup python anchor_mongo.py --min_id 130000000 --max_id 140000000 &
-nohup python anchor_mongo.py --min_id 140000000 --max_id 141000000 &
-nohup python anchor_mongo.py --min_id 142000000 --max_id 143000000 &
-nohup python anchor_mongo.py --min_id 150000000 --max_id 151000000 &
-nohup python anchor_mongo.py --min_id 152000000 --max_id 153000000 &
-nohup python anchor_mongo.py --min_id 154000000 --max_id 155000000 &
-nohup python anchor_mongo.py --min_id 156000000 --max_id 157000000 &
-nohup python anchor_mongo.py --min_id 158000000 --max_id 159000000 &
--- work/JK/relatives/corpus/go.py
+++
@@ -1,7 +0,0 @@
-import codecs
-fpw = codecs.open("./jk_lexicon.txt", "a+")
-fpr = codecs.open("./k_symptom.txt", "r")
-for line in fpr:
-    keyword = line.split()[1].strip().replace("\r\n", "")
-    fpw.write("%s\r\n" % (keyword))
-
